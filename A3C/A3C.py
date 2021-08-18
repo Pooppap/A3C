a57f5bf874032580a849cc7ee6762226279137f9
Chaiyawan A <ca1216@ic.ac.uk>
2021-08-18 14:47:52 +0100

    Update prototype

--- A3C/A3C.py
+++ A3C/A3C.py
@@ -1,8 +1,13 @@
-import gym
+import time
+import torch
 
+from gym import spaces
 from copy import deepcopy
 from stable_baselines3 import A2C
+from stable_baselines3.common.utils import safe_mean
+from stable_baselines3.common.utils import obs_as_tensor
 from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.utils import explained_variance
 
 
 class A3C(A2C):
@@ -11,9 +16,17 @@ class A3C(A2C):
         *args,
         **kwargs
     ):
-        super().__init__(*args, kwargs)
-        self.shared_policy = deepcopy(self.policy)
-        self.shared_policy.share_memory()
+        shared_policy_set = False
+        if "shared_policy" in kwargs:
+            kwargs["policy"] = deepcopy(kwargs["shared_policy"])
+            self.shared_policy = kwargs["shared_policy"]
+            _ = kwargs.pop("shared_policy")
+            shared_policy_set = True
+
+        super().__init__(*args, **kwargs)
+        if not shared_policy_set:
+            self.shared_policy = deepcopy(self.policy)
+            self.shared_policy.share_memory()
 
     def ensure_shared_grads(self):
         for param, shared_param in zip(self.policy.parameters(), self.shared_policy.parameters()):
@@ -23,4 +36,118 @@ class A3C(A2C):
             # Correctly cast param to the correct device.
             shared_param._grad = param.grad.to(shared_param.device)
 
+    def learn(self, *args, **kwargs):
+        torch.multiprocessing.spawn()
+        pass
 
+
+    def _train(self):
+        # Switch to train mode (this affects batch norm / dropout)
+        self.policy.train()
+
+        # Update optimizer learning rate
+        self._update_learning_rate(self.policy.optimizer)
+
+        # This will only loop once (get all data in one go)
+        for rollout_data in self.rollout_buffer.get(batch_size=None):
+
+            actions = rollout_data.actions
+            if isinstance(self.action_space, spaces.Discrete):
+                # Convert discrete action from float to long
+                actions = actions.long().flatten()
+
+            # TODO: avoid second computation of everything because of the gradient
+            values, log_prob, entropy = self.policy.evaluate_actions(rollout_data.observations, actions)
+            values = values.flatten()
+
+            # Normalize advantage (not present in the original implementation)
+            advantages = rollout_data.advantages
+            if self.normalize_advantage:
+                advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-8)
+
+            # Policy gradient loss
+            policy_loss = -(advantages * log_prob).mean()
+
+            # Value loss using the TD(gae_lambda) target
+            value_loss = torch.nn.functional.mse_loss(rollout_data.returns, values)
+
+            # Entropy loss favor exploration
+            if entropy is None:
+                # Approximate entropy when no analytical form
+                entropy_loss = -torch.mean(-log_prob)
+            else:
+                entropy_loss = -torch.mean(entropy)
+
+            loss = policy_loss + self.ent_coef * entropy_loss + self.vf_coef * value_loss
+
+            # Optimization step
+            self.policy.optimizer.zero_grad()
+            loss.backward()
+
+            # Clip grad norm
+            torch.nn.utils.clip_grad_norm_(self.policy.parameters(), self.max_grad_norm)
+            self.policy.optimizer.step()
+
+        explained_var = explained_variance(self.rollout_buffer.values.flatten(), self.rollout_buffer.returns.flatten())
+
+        self._n_updates += 1
+        self.logger.record("train/n_updates", self._n_updates, exclude="tensorboard")
+        self.logger.record("train/explained_variance", explained_var)
+        self.logger.record("train/entropy_loss", entropy_loss.item())
+        self.logger.record("train/policy_loss", policy_loss.item())
+        self.logger.record("train/value_loss", value_loss.item())
+        if hasattr(self.policy, "log_std"):
+            self.logger.record("train/std", torch.exp(self.policy.log_std).mean().item())
+
+    def _learn(
+        self,
+        total_timesteps,
+        callback=None,
+        log_interval=1,
+        eval_env=None,
+        eval_freq=-1,
+        n_eval_episodes=5,
+        tb_log_name="OnPolicyAlgorithm",
+        eval_log_path=None,
+        reset_num_timesteps=True,
+    ):
+        iteration = 0
+
+        total_timesteps, callback = self._setup_learn(
+            total_timesteps, eval_env, callback, eval_freq, n_eval_episodes, eval_log_path, reset_num_timesteps, tb_log_name
+        )
+
+        callback.on_training_start(locals(), globals())
+
+        while self.num_timesteps < total_timesteps:
+
+            continue_training = self.collect_rollouts(self.env, callback, self.rollout_buffer, n_rollout_steps=self.n_steps)
+
+            if continue_training is False:
+                break
+
+            iteration += 1
+            self._update_current_progress_remaining(self.num_timesteps, total_timesteps)
+
+            # Display training infos
+            if log_interval is not None and iteration % log_interval == 0:
+                fps = int(self.num_timesteps / (time.time() - self.start_time))
+                self.logger.record("time/iterations", iteration, exclude="tensorboard")
+                if len(self.ep_info_buffer) > 0 and len(self.ep_info_buffer[0]) > 0:
+                    self.logger.record("rollout/ep_rew_mean", safe_mean([ep_info["r"] for ep_info in self.ep_info_buffer]))
+                    self.logger.record("rollout/ep_len_mean", safe_mean([ep_info["l"] for ep_info in self.ep_info_buffer]))
+                self.logger.record("time/fps", fps)
+                self.logger.record("time/time_elapsed", int(time.time() - self.start_time), exclude="tensorboard")
+                self.logger.record("time/total_timesteps", self.num_timesteps, exclude="tensorboard")
+                self.logger.dump(step=self.num_timesteps)
+
+            self.train()
+
+        callback.on_training_end()
+
+        return self
+
+    def _get_torch_save_params(self):
+        state_dicts = ["shared_policy", "shared_policy.optimizer"]
+
+        return state_dicts, []

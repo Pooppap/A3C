c5a1ef1dc0a5f30bcec7dbd3522a6ca15cca7d25
Chaiyawan A <ca1216@ic.ac.uk>
2021-08-19 00:33:52 +0100

    Finish A3C model (probably)

--- A3C/A3C.py
+++ A3C/A3C.py
@@ -1,32 +1,48 @@
+import os
 import time
 import torch
 
 from gym import spaces
 from copy import deepcopy
 from stable_baselines3 import A2C
+from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.utils import safe_mean
 from stable_baselines3.common.utils import obs_as_tensor
 from stable_baselines3.common.env_util import make_vec_env
-from stable_baselines3.common.utils import explained_variance
+from stable_baselines3.common.utils import explained_variance 
 
 
 class A3C(A2C):
     def __init__(
         self,
+        policy,
+        env,
         *args,
         **kwargs
     ):
         shared_policy_set = False
         if "shared_policy" in kwargs:
             kwargs["policy"] = deepcopy(kwargs["shared_policy"])
-            self.shared_policy = kwargs["shared_policy"]
-            _ = kwargs.pop("shared_policy")
+            self.shared_policy = kwargs.pop("shared_policy")
+            # self.shared_policy.optimizer = torch.optim.Adam(self.shared_policy.parameters(), lr=1e-5) # shift optimizer to child memory space
+
             shared_policy_set = True
 
-        super().__init__(*args, **kwargs)
+        if "base_log_dir" in kwargs:
+            self.base_log_dir = kwargs.pop("base_log_dir")
+        else:
+            self.base_log_dir = None
+
+        self._parent_args = args
+        self._parent_kwargs = kwargs
+
+        super().__init__(policy, env, *args, **kwargs)
         if not shared_policy_set:
             self.shared_policy = deepcopy(self.policy)
+            # del self.shared_policy.optimizer # No need for global optimizer
             self.shared_policy.share_memory()
+        # else:
+        #     del self.policy.optimizer
 
     def ensure_shared_grads(self):
         for param, shared_param in zip(self.policy.parameters(), self.shared_policy.parameters()):
@@ -36,9 +52,25 @@ class A3C(A2C):
             # Correctly cast param to the correct device.
             shared_param._grad = param.grad.to(shared_param.device)
 
+    def _multiprocess_learning(self, rank, *args):
+        host_torch_seed = self.seed if self.seed else torch.seed
+        host_torch_seed += rank
+
+        env = deepcopy(self.env)
+        env = Monitor(env, filename=os.path.join(self.base_log_dir, f"Process_{rank}"))
+
+        model = A3C(deepcopy(self.policy), env, *self._parent_args, shared_policy=self.shared_policy, **self._parent_kwargs)
+        model._learn(*args, **self._learn_kwargs)
+
     def learn(self, *args, **kwargs):
-        torch.multiprocessing.spawn()
-        pass
+        nprocs = kwargs.pop("nprocs", 1)
+        lock = torch.multiprocessing.Lock()
+        counter = torch.multiprocessing.Value('i', 0)
+
+        kwargs["is_child"] = True
+        self._learn_kwargs = kwargs
+        args = [lock, counter] + args
+        torch.multiprocessing.spawn(self._multiprocess_learning, args=args, nprocs=nprocs)
 
 
     def _train(self):
@@ -46,7 +78,7 @@ class A3C(A2C):
         self.policy.train()
 
         # Update optimizer learning rate
-        self._update_learning_rate(self.policy.optimizer)
+        self._update_learning_rate(self.shared_policy.optimizer)
 
         # This will only loop once (get all data in one go)
         for rollout_data in self.rollout_buffer.get(batch_size=None):
@@ -81,12 +113,13 @@ class A3C(A2C):
             loss = policy_loss + self.ent_coef * entropy_loss + self.vf_coef * value_loss
 
             # Optimization step
-            self.policy.optimizer.zero_grad()
+            self.shared_policy.optimizer.zero_grad()
             loss.backward()
 
             # Clip grad norm
             torch.nn.utils.clip_grad_norm_(self.policy.parameters(), self.max_grad_norm)
-            self.policy.optimizer.step()
+            self.ensure_shared_grads()
+            self.shared_policy.optimizer.step()
 
         explained_var = explained_variance(self.rollout_buffer.values.flatten(), self.rollout_buffer.returns.flatten())
 
@@ -101,6 +134,8 @@ class A3C(A2C):
 
     def _learn(
         self,
+        lock,
+        counter,
         total_timesteps,
         callback=None,
         log_interval=1,
@@ -121,7 +156,12 @@ class A3C(A2C):
 
         while self.num_timesteps < total_timesteps:
 
+            self.policy.load_state_dict(self.shared_policy.state_dict())
+
             continue_training = self.collect_rollouts(self.env, callback, self.rollout_buffer, n_rollout_steps=self.n_steps)
+            
+            with lock:
+                counter.value = self.num_timesteps
 
             if continue_training is False:
                 break
